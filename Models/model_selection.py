f67de6472ac793123eba5c0319f9a8af288f2406
YimingZhao-art <78484762+YimingZhao-art@users.noreply.github.com>
2024-11-15 14:03:02 +0100

    0.852

--- Models/model_selection.py
+++ Models/model_selection.py
@@ -51,30 +51,32 @@ sys.path.append(current_dir + "/../FeaturePreprocessing/")
 from utils import *
 from FeaturePreprocessing.process import *
 
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 models = {
-    # "GaussianProcess": GaussianProcessClassifier(kernel=RBF(), n_jobs=-1),
-    "LGBM": LGBMClassifier(n_jobs=-1),
+    "GaussianProcess": GaussianProcessClassifier(kernel=RBF(), n_jobs=-1),
+    "LGBM": LGBMClassifier(n_jobs=-1, verbose=-1),
     "XGB": XGBClassifier(n_jobs=-1),
-    "CatBoost": CatBoostClassifier(),
-    # "GradientBoosting": GradientBoostingClassifier(verbose=2), # too slow
-    # "AdaBoost": AdaBoostClassifier(),
+    "CatBoost": CatBoostClassifier(verbose=0),
+    "GradientBoosting": GradientBoostingClassifier(verbose=0),  # too slow
+    "AdaBoost": AdaBoostClassifier(),
     "ExtraTrees": ExtraTreesClassifier(),
     "Bagging": BaggingClassifier(),
     "RandomForest": RandomForestClassifier(),
     "LogisticRegression": LogisticRegression(),
-    # "Ridge": RidgeClassifier(),
-    # "SGD": SGDClassifier(),
-    # "PassiveAggressive": PassiveAggressiveClassifier(),
-    # "SVC": SVC(),
+    "Ridge": RidgeClassifier(),
+    "SGD": SGDClassifier(),
+    "PassiveAggressive": PassiveAggressiveClassifier(),
+    "SVC": SVC(),
     "DecisionTree": DecisionTreeClassifier(),
-    # "KNeighbors": KNeighborsClassifier(),
-    # "GaussianNB": GaussianNB(),
+    "KNeighbors": KNeighborsClassifier(),
+    "GaussianNB": GaussianNB(),
     "MLP": MLPClassifier(),
 }
 
 
+@time_wrapper
 def try_models(
     X_train: pd.DataFrame,
     y_train: pd.Series,
@@ -100,7 +102,7 @@ def try_models(
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         scores[name] = f1_score(
-            y_test, y_pred, average="weighted"
+            y_test, y_pred, average="micro"
         )  # since it's a multiclass classification problem
         end_time = time.time()
         print(f"{name}: {scores[name]} in {end_time-start_time:.2f} seconds")
@@ -108,11 +110,20 @@ def try_models(
         # clear resources, if possible
         del model
 
+    with open(os.path.join(current_dir, "scores.txt"), "w") as f:
+        for name, score in scores.items():
+            f.write(f"{name}: {score:.4f}\n")
+
     return scores
 
 
 def main():
-    X_train, y_train, X_test = processWithStrategy(warmstart=True)
+    X_train = pd.read_csv('Data/final/final_X_train.csv', header=None).iloc[1:, 1:]
+    X_test = pd.read_csv('Data/final/final_X_test.csv', header=None).iloc[1:, 1:]
+    y_train = pd.read_csv('Data/final/final_y_train.csv', header=None).iloc[1:, 1:]
+
+    
+    assert X_test.shape[0] == 3411 and X_train.shape[1] == X_test.shape[1], "Data shape error"
 
     y_train = y_train.values.ravel()
 
@@ -120,33 +131,58 @@ def main():
         scores = f.read()
         scores = [score.split(": ") for score in scores.split("\n") if score != ""]
         scores = {
-            score[0]: float(score[1]) for score in scores if float(score[1]) > 0.6
+            score[0]: float(score[1]) for score in scores if float(score[1]) > 0.86
         }
-    
+
     def calculate_weights(scores, base=3.0, delta=0.02):
         min_score = min(scores)
         weights = np.array([base ** ((s - min_score) / delta) for s in scores])
         weights /= np.sum(weights)
         return weights
-    
+
     print("Scores: ", scores)
-    s = [scores[name] for name in models.keys()]
+    s = [scores[name] for name in scores.keys()]
     weights = calculate_weights(s)
     print("Weights: ", weights)
 
     # 确保 estimators 和 weights 的模型名称一致
-    estimators = [(name, models[name]) for name in models.keys()]
-    # weights = [scores[name] for name in models.keys()]
+    estimators = [(name, models[name]) for name in scores.keys()]
+    stack = StackingClassifier(estimators=estimators)
     voting = VotingClassifier(estimators=estimators, voting="soft", weights=weights)
+
+    # print_boundary("cv for voting", fill_char="*")
+    # start_time = time.time()
+    # # make f1 scorer
+    # f1_scorer = make_scorer(f1_score, average="micro")
+    # cv_score = cross_val_score(
+    #     voting, X_train, y_train, cv=5, scoring=f1_scorer, n_jobs=-1
+    # )
+    # print("Cross validation score: ", cv_score)
+    # print("Mean cross validation score: ", np.mean(cv_score))
+    # end_time = time.time()
+    # print_boundary(f"used {start_time-end_time}", fill_char="*")
+
+    # print_boundary("cv for stacking", fill_char="*")
+    # start_time = time.time()
+    # cv_score2 = cross_val_score(
+    #     stack, X_train, y_train, cv=5, scoring=f1_scorer, n_jobs=-1
+    # )
+    # print("Cross validation score: ", cv_score2)
+    # print("Mean cross validation score: ", np.mean(cv_score2))
+    # end_time = time.time()
+    # print_boundary(f"used {start_time-end_time}", fill_char="*")
+    
+    # exit()
 
     print("Train the model...", estimators)
 
     @time_wrapper
-    def train():
+    def train(model):
         # train on the whole dataset
-        voting.fit(X_train, y_train)
+        model.fit(X_train, y_train)
+        return model
 
-    train()
+    voting = train(voting)
     # predict on the test set
     y_pred = voting.predict(X_test)
     submit_pred(y_pred, "softvote.csv")
